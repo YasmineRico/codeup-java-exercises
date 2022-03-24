1f7f45cc7f62d7c5a71a54e37e2d4ee32943fae1
Yasmine Rico <ricolyasmine@gmail.com>
2022-03-24 16:29:46 -0500

    complete JAVA exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,72 @@
 public class HelloWorld { // java code has to be inside of a class and then inside of a main method.
 
     //this is called a method. all java code has to have a main method.
-    public static void main (String[] args){
+    public static void main(String[] args) {
         System.out.println("Hello World");
+        int myFavoriteNumber = 14;
+        System.out.println("myFavoriteNumber = " + myFavoriteNumber);
+        
+        String myString = "I love puppies";
+        System.out.println("myString = " + myString);
+        
+        myString = "happy days!";
+        System.out.println("myString = " + myString);
+        
+        myString = "3.14159";
+        System.out.println("myString = " + myString);
+        
+//        long myNumber =
+//        System.out.println("myNumber = " + myNumber);
+        
+//        long myNumber = 3.14;
+//        System.out.println("myNumber = " + myNumber);
+        
+//       long myNumber = 123L;
+//        System.out.println("myNumber = " + myNumber);
+
+//        myNumber= 123;
+//        System.out.println("myNumber = " + myNumber);
+
+        float myNumber = 3.14F;
+        System.out.println("myNumber = " + myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        int class = // you cannot call a variable a class.
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+////        int three = (int) o;
+////        System.out.println("three = " + three);
+//
+//        int three = (int) "three";
+//        System.out.println("three = " + three);
+
+//        int x = 4;
+////        x = x + 5;
+//        x += 5;
+//        System.out.println("x = " + x);
+
+//        int x = 3;
+//        int y = 4;
+////        y = y * x;
+//        y *=  x;
+//        System.out.println("y = " + y);
+
+        int x = 10;
+        int y = 2;
+//        x = x / y;
+//        y = y - x;
+        x /= y;
+        System.out.println("x = " + x);
+        y -= x;
+        System.out.println("y = " + y);
+
     }// end of main method
 }
---
+++ src/YoHey.java
@@ -0,0 +1,38 @@
+public class YoHey {
+    public static void main(String[] args) { // main -- tab
+        System.out.println("Yo, hey!!"); //sout -- tab this equivalent to console.log
+    }
+}
+//You can do this in the command line to check out what is in your file
+
+//cd into your scr and then check the files inside by doing ls --> then do javac fileName.java --> the run java filename without the .java. It should output the same info as when you run the YoHey Class through intelliJ
+
+//in JAVA you wil always need to use semicolons otherwise it will not work.
+
+//sstatments are lines of code terminated with semicolons in js you didnt always need to put that but in java you do,.
+
+//DATATYPES: primative data types and reference. primative is built into java and can be built into the reference data type. Object use primitave data types and then create their own ovject reference. Total of 8. Amount of memory may be a problem so yyou will need to ensure you are using the best amount of data type is better for your purposese.
+
+//variables must be defined before you used them, just like in JS. Start w. letter, underscore or dollar sign (java). consist of letters, and underscores and digits. But can't be more than 255 characters. initiatae and assigned value
+
+//CASTING-
+//JS -> parseInt() -> type cohersion ==== casting.
+//implicit casting means to not be too clear but it gives an idea of what should occcur. Explicit is telling the compu exactly what to do.
+
+
+
+//float limited to show seven digits and will round the last digit if needed. If you put too many digits than what your data type can hold you run the risk of not having it return exactly as you wanted it to. for example: 123456789 this will return 12345679..
+
+//single quotes for the char data set. in JAVA "" this means specifically a string.
+
+//in JS a boolean return true or false (1 or 0)
+
+//refereance dfault to a value of null if not asnged anything. made up of different refercernc typees. Escape characters. My string:"
+
+//certain words cant be used outuside of function they were defined by the language for.
+
+//for continue, new, switch.... many more, class
+
+// JAVA uses a kreyword.
+
+//to define a constatnt we define a variable and then we do something else. When you get home maybe take a nappies?
