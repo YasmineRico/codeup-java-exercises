d165c5e65cc9d6fa7ed847cd0ff96490ab4e122b
Yasmine Rico <ricolyasmine@gmail.com>
2022-03-24 11:38:02 -0500

    Create Hello World program and add gitignore

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld { // java code has to be inside of a class and then inside of a main method.
+
+    //this is called a method. all java code has to have a main method.
+    public static void main (String[] args){
+        System.out.println("Hello World");
+    }// end of main method
+}
