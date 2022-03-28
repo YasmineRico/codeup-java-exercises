71923636a2ffb110abd0d093768400a678b126ae
Yasmine Rico <ricolyasmine@gmail.com>
2022-03-28 15:50:30 -0500

    complete exercise 1.b.1

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,10 +1,14 @@
 public class ControlFlowExercises {
     public static void main (String [] args) {
-        int i = 5;
-        while(i <= 15){
-            System.out.println(i);
+//        int i = 5;
+//        while(i <= 15){
+//            System.out.println(i);
+//            i++;
+//        }
+        int i = 0;
+        do{
+            System.out.println(i *2);
             i++;
-        }
-
+        }while (i <= 50);
     }
 }
