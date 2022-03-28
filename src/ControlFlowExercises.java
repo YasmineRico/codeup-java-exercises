d7fc968c8d64fef69565539d4e6b125a3f404b0a
Yasmine Rico <ricolyasmine@gmail.com>
2022-03-28 16:07:55 -0500

    complete exercise 1.b.2

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -5,10 +5,10 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //            i++;
 //        }
-        int i = 0;
+        int i = 100;
         do{
-            System.out.println(i *2);
-            i++;
-        }while (i <= 50);
+            System.out.println(i);
+            i-= 5;
+        }while (i >= -10);
     }
 }
