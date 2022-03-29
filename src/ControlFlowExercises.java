30dab89c034467d3f08f8ce66b11bfa379731cf4
Yasmine Rico <ricolyasmine@gmail.com>
2022-03-29 09:46:44 -0500

    complete FizzBuzz exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -11,8 +11,25 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //            i *= i;
 //        }while (i <= 1000000);
-        for (long i =2; i < 1000000; i *=i){
-            System.out.println(i);
+//        for (long i =2; i < 1000000; i *=i){
+//            System.out.println(i);
+//        }
+      int a = 3;
+        int b = 5;
+        for (int i =1; i <= 100; i ++){
+            String s = "";
+            if (i ==a){
+                a =a +3;
+                s = s + "Fizz";
+            }
+            if (i == b){
+                b= b+5;
+                s = s +"Buzz";
+            }
+            if (s == "")
+                System.out.println(i);
+            else System.out.println(s);
         }
+
     }
 }
