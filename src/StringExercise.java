4b4ba1df1dd5f646e73dacd7306693228cac34d4
Yasmine Rico <ricolyasmine@gmail.com>
2022-03-29 10:57:10 -0500

    complete first exercise

---
+++ src/StringExercise.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class StringExercise {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        String message = "We don't need no education";
+        System.out.println(message);
+        String saying ="We don't need no thought control";
+        System.out.println(saying);
+        sc.nextLine();
+        String happy= "Check \"this\" out!, \"s inside of\" s!";
+        System.out.println(happy);
+        sc.nextLine();
+        String window = "In windows, the main drive is usually C:\\";
+        System.out.println(window);
+        sc.nextLine();
+        String backSlash = "I can do backslashes \\, double backslashes \\\\";
+        System.out.println(backSlash);
+        String tripleSlash = "and the amazing triple backslash \\\\\\!";
+        System.out.println(tripleSlash);
+    }
+}
