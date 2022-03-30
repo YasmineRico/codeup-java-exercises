2f15c92dc4c834d2be2e219305b1cdcd47805f72
Yasmine Rico <ricolyasmine@gmail.com>
2022-03-30 12:26:09 -0500

    add recursion solution to multiplication method

--- src/MethodExercises.java
+++ src/MethodExercises.java
@@ -20,13 +20,17 @@ public static int addition (int num1, int num2){
 public static int subtraction(int num1, int num2){
     return num1 - num2;
 }
-public static int multiplication(int num1, int num2){
-   int result = 0;
-   for (int i =0; i < num1; i ++){
-       result += num2;
-   }
-   return result;
+public static int multiplication(int num1, int num2) {
+    if (num1 == 0 || num2 == 0) {
+        return 0;
+    } else if (num2 < 0) {
+        return -num1 + multiplication(num1, num2 + 1);
+    }
+    else{
+        return num1+ multiplication(num1, num2 -1);
+    }
 }
+
 public static int division(int num1, int num2){
     return num1 / num2;
 }
@@ -37,7 +41,7 @@ public static int modulus(int num1, int num2){
 public static void main(String[] args){
     System.out.println(addition(1, 2));
     System.out.println(subtraction(9, 5));
-    System.out.println(multiplication(4, 4));
+    System.out.println(multiplication(5, 4));
     System.out.println(division(8, 4));
     System.out.println(modulus(15,6));
 
