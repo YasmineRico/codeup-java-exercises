0dec7f233b47c998ae8a601544343b94c3a98363
Yasmine Rico <ricolyasmine@gmail.com>
2022-03-30 10:39:31 -0500

    replace empty string with isEmpty, add end of chat line

--- src/Bob.java
+++ src/Bob.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+
+
 public class Bob {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -7,11 +9,14 @@ public class Bob {
 
             if (userInput.endsWith("!")) {
                 System.out.println("Woah, chill out!");
+
             } else if (userInput.endsWith("?")) {
                 System.out.println("Sure");
-            } else if (userInput.equals("")) {
+            } else if (userInput.isEmpty()) {
                 System.out.println("Fine. Be that way!");
             } else
                 System.out.println("Whatever.");
+            System.out.print("Bob got bored and left the chat....");
     }
+
 }
