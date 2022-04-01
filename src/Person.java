d48570d5e62524b93b9d1875ce129bea1cd9c039
Yasmine Rico <ricolyasmine@gmail.com>
2022-04-01 11:19:26 -0500

    complete exercise

--- src/Person.java
+++ src/Person.java
@@ -1,7 +1,8 @@
 
 
 public class Person {
-    private String name;
+  private String name;
+
     public String getName(){
 //TODO: return the person's name
         return this.name;
@@ -9,16 +10,34 @@ public class Person {
 
     public void setName(String name){
 //TODO: change the name field to the passed value
-        this.name=name;
+        this.name =name;
     }
     public void sayHello(){
 //TODO: print a message to the console using the person's name
-        System.out.println("Hello "+ name);
+        System.out.println("Hello"+name);
+    }
+    //The class should have a constructor that accepts a `String` value and sets
+    //the person's name to the passed string.
+    public Person (String name){
+        this.name=name;
     }
 
+//Create a `main` method on the class that creates a new `Person` object and tests the above methods.
     public static void main(String[] args) {
-        Person person1 = new Person();
-        person1.sayHello();
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        //Compares the persons to see if they hold the same value
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        //is false they are not the same "person" just the same value
+//        System.out.println(person1 == person2);
+
+        Person person1 = new Person("John");
+        Person person2 = person1;
         System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        person2.setName("Jane");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+
     }
 }
