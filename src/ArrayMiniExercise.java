e632ca3d0067ccc5d53acd57e04856755a514783
Yasmine Rico <ricolyasmine@gmail.com>
2022-04-04 09:58:03 -0500

    complete first array mini exercise

---
+++ src/ArrayMiniExercise.java
@@ -0,0 +1,12 @@
+public class ArrayMiniExercise {
+    public static void main(String[] args) {
+
+String [] familyMembers = {"Thomas", "Takito", "Evie", "Stella"};
+        System.out.println(familyMembers[3]);
+        for(String familyMember: familyMembers){
+            System.out.println(familyMember);
+        }
+
+    }
+
+}
