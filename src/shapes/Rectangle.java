d5c1e0aef6c9d021adae9dcf35e9c08cdb60c9aa
Yasmine Rico <ricolyasmine@gmail.com>
2022-04-05 10:38:55 -0500

    begin working on overriding

---
+++ src/shapes/Rectangle.java
@@ -0,0 +1,23 @@
+package shapes;
+
+public class Rectangle {
+    public int length;
+    public int width;
+
+    public Rectangle (int length, int width){
+        this.length = length;
+        this.width = width;
+
+    }
+
+    public int getArea(){
+        return this.width * this.length;
+    }
+    public int getPerimeter(){
+        return (2*this.length) + (2*this.width);
+    }
+
+
+
+
+}
---
+++ src/shapes/Square.java
@@ -0,0 +1,18 @@
+package shapes;
+
+public class Square extends Rectangle{
+    public int side;
+    public Square (int side){
+        super(side, side);
+        this.side =side;
+    }
+    public int getArea(){
+        return side * side;
+    }
+
+    public int getPerimeter(){
+        return side *4;
+    }
+
+
+}
