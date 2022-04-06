ca3a7fb261ac151adc3ce0d3476947bfe46686a0
Yasmine Rico <ricolyasmine@gmail.com>
2022-04-06 15:09:18 -0500

    complete first portion of the exercise

---
+++ src/grades/Student.java
@@ -0,0 +1,40 @@
+package grades;
+
+import java.util.ArrayList;
+
+public class Student {
+    private String name;
+    private ArrayList<Integer> grades;
+
+    public Student(String name){
+        this.name= name;
+        grades = new ArrayList<>();
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void addGrade(int grade){
+        grades.add(grade);
+    }
+
+    public double getGradeAverage(){
+        int total = 0;
+        for (Integer grade: grades) total += grade;
+        return total/grades.size();
+    }
+
+    public static void main(String[] args) {
+        Student thomas = new Student("Thomas");
+        thomas.getName();
+        thomas.addGrade(100);
+        thomas.addGrade(80);
+        thomas.addGrade(75);
+        System.out.println(thomas.getName());
+        System.out.println(thomas.getGradeAverage());
+    }
+
+}
+
+
