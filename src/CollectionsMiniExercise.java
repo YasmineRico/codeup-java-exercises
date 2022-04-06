8515e18328639f4c0186d111b387c8b1c72575fc
Yasmine Rico <ricolyasmine@gmail.com>
2022-04-06 11:26:11 -0500

    complete mini exercise

---
+++ src/CollectionsMiniExercise.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class CollectionsMiniExercise {
+    public static void main(String[] args) {
+        ArrayList<Person> persons = new ArrayList<>();
+        Person thomas = new Person ("Thomas");
+        Person takito = new Person("Takito");
+        Person evie = new Person ("Evie");
+        persons.add(thomas);
+        persons.add(takito);
+        persons.add(evie);
+        for(Person person: persons){
+            System.out.println(person.getName());
+        }
+
+    }
+
+
+}
